a3a421d4bf0f85927217a25923f841f18f60a603
Yvo Rolefes <yvo@earlyharvest.nl>
2023-06-05 15:12:01 +0200

    Update index.js

--- public_html/index.js
+++ public_html/index.js
@@ -9,11 +9,19 @@ webSocket.on('connection', wsClient => {
     wsClient.on('message', messageData => {
         console.log('Message received: ' + messageData);
         clients.forEach(client => {
-            client.broadcast(JSON.stringify({func:messageData}));
-        });
+            client.broadcast(JSON.stringify({func:messageData}))
+        })
     });
+
     wsClient.on('close', () => {
         console.log('Client disconnected');
         clients.splice(clients.indexOf(wsClient), 1);
     });
 })
+webSocket.broadcast = function broadcast(messageData) {
+    webSocket.clients.forEach(function each(client) {
+        if (client.readyState === socket.OPEN) {
+            client.send(messageData);
+        }
+    });
+};
